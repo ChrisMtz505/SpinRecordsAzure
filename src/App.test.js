cc5557528d4538e5933ae6ac1e2285b6e84f0dd0
ChrisMtz505 <cristoelpipo@gmail.com>
2024-12-07 14:57:02 -0600

    Otra

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,6 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
 test('renders bienvenida a la tienda text', () => {
   render(<App />);
   const linkElement = screen.getByText(/Bienvenido a nuestra Tienda/i);
